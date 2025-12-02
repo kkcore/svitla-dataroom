d58789b9c1772f975e0f8966940de274201fbb55
kkcore <oszust.jakub@gmail.com>
2025-12-02 02:55:57 +0100

    Add script in main.py as railway is using python main.py

--- backend/main.py
+++ backend/main.py
@@ -31,3 +31,12 @@ app.add_middleware(
 # Register routers
 app.include_router(auth.router)
 app.include_router(files.router)
+
+
+if __name__ == "__main__":
+    import os
+
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
